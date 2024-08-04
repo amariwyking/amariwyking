e8338b90b38c703f582af52ad76adcd007ec9e43
Amari Garrett <amariwgarrett@gmail.com>
2024-08-04 01:19:54 -0600

    Make construction message responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center place-content-center p-24">
-      <p className="text-5xl font-mono">amari wyking</p>
-      <span className="type-construction text-gray-500 m-4"></span>
+    <main className="flex min-h-screen flex-col place-content-center p-8">
+      <div className="items-stretch">
+        <p className="block text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-mono text-center">amari wyking</p>
+        <span className="block text-sm sm:text-l md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl type-construction text-gray-500 my-4 text-center"></span>
+      </div>
     </main>
   );
 }
