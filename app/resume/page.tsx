a5f7e8afd682cb68844df5aa21e6ae158f09b1a6
Amari Garrett <amariwgarrett@gmail.com>
2024-09-07 18:03:48 -0400

    Create resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <main className="w-screen h-screen">
+            <div className="max-w-[850px] max-h-[1100px] mx-auto my-auto invert">
+                <iframe src="https://resume.creddle.io/embed/c5gmqtmf0he" width="850" height="1100" seamless></iframe>
+            </div>
+        </main>
+
+    )
+}
