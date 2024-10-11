7203030dc04c56a865038dc0009e7668feea75ec
Amari Garrett <amariwgarrett@gmail.com>
2024-10-11 17:34:56 -0400

    Implement src Directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,8 @@ const withMT = require("@material-tailwind/react/utils/withMT")
 
 module.exports = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontSize: {
