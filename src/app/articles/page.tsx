a27806edb3ea21901ef7c8312c9412d462650f80
Amari Garrett <amariwgarrett@gmail.com>
2024-12-30 23:14:31 -0500

    Hotfix: Add keys to the ArticleCard component list items

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -13,16 +13,19 @@ const description =
     `
 const articlesData = [
     {
+        id: '00',
         title: 'Spatial and Temporal Distributions of Chicago Crimes',
         description: 'In this article I demonstrate how k-means and Gaussian mixture clustering methods can be used to identify how various types of crime display different trends over space and time in Chicago, IL.',
         date: '2024-04-16'
     },
     {
+        id: '01',
         title: 'The Gender-Data Gap: How Design Fails Half of Humanity',
         description: 'Picture a world where every product, service, and system was designed with only half of humanity in mind. Now consider this hypothetical scenario is closer to fact than it is to fiction.',
         date: '2024-11-19'
     },
     {
+        id: '02',
         title: 'Equity in Education: Bridging Funding Disparities for a Prosperous America',
         description: 'Quality education serves as the bedrock of prosperous and equitable communities, fostering the capacity to address local, national, and global challenges effectively.',
         date: '2023-09-29'
@@ -30,7 +33,7 @@ const articlesData = [
 ];
 
 const articleCards = articlesData.map((articleProps) => (
-    <ArticleCard {...articleProps} />
+    <ArticleCard key={articleProps.id} {...articleProps} />
 ))
 
 export default function ArticlesPage() {
