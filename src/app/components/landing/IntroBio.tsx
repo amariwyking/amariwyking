5128c07846cda96bdb682bdbd8d574cb40f7296d
Amari Garrett <amariwgarrett@gmail.com>
2025-06-14 17:31:20 -0400

    Fix the unnecessary backticks used for the escape character

--- src/app/components/landing/IntroBio.tsx
+++ src/app/components/landing/IntroBio.tsx
@@ -4,7 +4,7 @@ export default function IntroBio() {
     return (
         <div className="justify-self-start mx-auto col-span-1 md:col-span-2 order-3">
             <p className="font-work-sans mx-8 md:mx-auto mt-6 text-left text-base text-gray-600 md:text-lg lg:text-2xl ">
-                I`&apos;`m a software engineer with a strong focus on data-driven systems and urban technology. My background spans full-stack development, mobile applications, and machine learning, shaped by industry experience at leading tech companies and graduate research at the NYU Center for Urban Science + Progress.
+                I&apos;m a software engineer with a strong focus on data-driven systems and urban technology. My background spans full-stack development, mobile applications, and machine learning, shaped by industry experience at leading tech companies and graduate research at the NYU Center for Urban Science + Progress.
                 <br />
                 I bring a product-oriented mindset to technical challenges, with a particular interest in building scalable tools that improve how complex systems—especially cities—operate and adapt. Whether it be digital products or researching urban problems, I love digging into problems that sit at the edge of software, data, and the real world.
             </p>
