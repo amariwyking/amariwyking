4f16660f0cbe2a20df57a92dd27467c4e03c934f
Amari Garrett <amariwgarrett@gmail.com>
2025-06-14 21:34:22 -0400

    Update image in intro section

--- src/app/components/landing/ProfilePic.tsx
+++ src/app/components/landing/ProfilePic.tsx
@@ -3,13 +3,20 @@ import Image from 'next/image'
 export default function ProfilePic() {
     return (
         <div className='flex items-center justify-center -order-1 md:-order-1'>
-            <Image
-                className="rounded-full w-48 md:w-64 lg:w-80 sm:ring-4 md:ring-8 ring-surface hover:ring-surface-dark transition duration-300 ease-in-out"
-                src='/general_images/amaricentralpark.jpg'
-                alt=""
-                width={280}
-                height={280}
-            />
+            <div className="relative w-48 md:w-64 lg:w-80 h-48 md:h-64 lg:h-80">
+                <Image
+                    className="w-full h-full object-cover"
+                    src='/general_images/suited.jpg'
+                    alt=""
+                    fill
+                />
+                <div 
+                    className="absolute inset-0 bg-contain bg-center"
+                    style={{
+                        backgroundImage: "url('/image_frame.svg')"
+                    }}
+                />
+            </div>
         </div>
     )
 }
