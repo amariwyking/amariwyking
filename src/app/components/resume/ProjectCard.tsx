c3a0850646bc60d3824ff1fb8b0be5682c72c28c
Amari Garrett <amariwgarrett@gmail.com>
2025-06-16 13:00:29 -0400

    Modify design of project cards

--- src/app/components/resume/ProjectCard.tsx
+++ src/app/components/resume/ProjectCard.tsx
@@ -7,13 +7,15 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <div className="w-full max-w-lg h-fit bg-white border border-gray-200 rounded-lg">
-      <div className="p-5">
-        <h5 className="mb-2 text-md font-kode-mono tracking-tight text-gray-900 ">{project.name}</h5>
-        <p className="mb-3 font-work-sans text-sm text-gray-700 line-clamp-1">
-          {project.description}
-        </p>
-      </div>
+    <div className="w-full h-fit max-w-lg py-6 px-12 font-kode-mono text-green-600 ring-4 ring-green-600">
+
+      <h5 className="text-md md:text-xl font-bold tracking-wider ">
+        {project.name}
+      </h5>
+      <div className="w-full h-[2px] my-2 bg-green-600"></div>
+      <p className="text-sm md:text-base tracking-tighter  line-clamp-2">
+        {project.description}
+      </p>
     </div>
   );
 }
